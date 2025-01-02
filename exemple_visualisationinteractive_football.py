cf53766fbaf4d1814a841ddd1b6d487bc16ddb4f
Rako75 <147698888+Rako75@users.noreply.github.com>
2025-01-02 20:09:53 +0100

    Update exemple_visualisationinteractive_football.py

--- exemple_visualisationinteractive_football.py
+++ exemple_visualisationinteractive_football.py
@@ -63,73 +63,145 @@ league_files = {
     },
 }
 
-# Streamlit application
-st.title("Comparaison de Joueurs - Football 2023")
-
-# Sélection des paramètres
-selected_position = st.selectbox("Choisissez la position", options=["Attaquant", "Défenseur", "Milieu"])
-league1 = st.selectbox("Sélectionnez la ligue du premier joueur", options=list(league_files.keys()))
-league2 = st.selectbox("Sélectionnez la ligue du deuxième joueur", options=list(league_files.keys()))
-
-# Chargement des données et des joueurs
-data1, params1 = load_and_preprocess_data(league_files[league1][selected_position], selected_position)
-data2, params2 = load_and_preprocess_data(league_files[league2][selected_position], selected_position)
-
-player1 = st.selectbox("Sélectionnez le premier joueur", options=data1['Joueur'].unique())
-player2 = st.selectbox("Sélectionnez le deuxième joueur", options=data2['Joueur'].unique())
-
-# Extraction des données des joueurs
-player1_data = data1[data1['Joueur'] == player1].iloc[0][params1].tolist()
-player2_data = data2[data2['Joueur'] == player2].iloc[0][params2].tolist()
-
-# Extraction du club et de l'âge des joueurs
-club1 = data1[data1['Joueur'] == player1].iloc[0]['Equipe']
-club2 = data2[data2['Joueur'] == player2].iloc[0]['Equipe']
-age1 = int(data1[data1['Joueur'] == player1].iloc[0]['Age'])
-age2 = int(data2[data2['Joueur'] == player2].iloc[0]['Age'])
-
-# Dictionnaire des logos des clubs (exemple avec quelques logos fictifs)
+# Dictionnaire des logos des clubs avec les liens directs GitHub
 club_logos = {
-    "Manchester United": "https://upload.wikimedia.org/wikipedia/commons/a/a3/Manchester_United_FC_crest.svg",
-    "Liverpool": "https://upload.wikimedia.org/wikipedia/commons/0/0a/Liverpool_FC_logo.png",
-    "FC Barcelona": "https://upload.wikimedia.org/wikipedia/commons/4/47/FC_Barcelona_logo_%282019%29.png",
-    # Ajouter d'autres clubs et leurs liens de logo ici
+    # Serie A
+    'Roma': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Roma.png',
+    'Inter': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Inter.png',
+    'Milan': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Milan.png',
+    'Bologna': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Bologna.png',
+    'Udinese': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Udinese.png',
+    'Monza': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Monza.png',
+    'Genoa': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Genoa.png',
+    'Lazio': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Lazio.png',
+    'Juventus': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Juventus.png',
+    'Lecce': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Lecce.png',
+    'Fiorentina': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Fiorentina.png',
+    'Hellas Verona': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Hellas Verona.png',
+    'Cagliari': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Cagliari.png',
+    'Frosinone': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Frosinone.png',
+    'Sassuolo': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Sassuolo.png',
+    'Atalanta': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Atalanta.png',
+    'Empoli': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Empoli.png',
+    'Salernitana': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Salernitana.png',
+    'Torino': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Torino.png',
+    'Napoli': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos/Napoli.png',
+    
+    # Premier League
+    'Bournemouth': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Bournemouth.png',
+    'Chelsea': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Chelsea.png',
+    'Fulham': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Fulham.png',
+    'Luton Town': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Luton Town.png',
+    'Brighton': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Brighton.png',
+    'West Ham': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/West Ham.png',
+    'Nott\'ham Forest': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Nott\'ham Forest.png',
+    'Crystal Palace': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Crystal Palace.png',
+    'Sheffield Utd': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Sheffield Utd.png',
+    'Wolves': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Wolves.png',
+    'Brentford': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Brentford.png',
+    'Manchester City': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Manchester City.png',
+    'Liverpool': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Liverpool.png',
+    'Newcastle Utd': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Newcastle Utd.png',
+    'Burnley': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Burnley.png',
+    'Manchester Utd': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Manchester Utd.png',
+    'Aston Villa': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Aston Villa.png',
+    'Tottenham': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Tottenham.png',
+    'Everton': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Everton.png',
+    'Arsenal': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Premier%20League%20Logos/Arsenal.png',
+    
+    # La Liga
+    'Getafe': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Getafe.png',
+    'Betis': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Betis.png',
+    'Alavés': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Alavés.png',
+    'Sevilla': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Sevilla.png',
+    'Athletic Club': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Athletic Club.png',
+    'Celta Vigo': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Celta Vigo.png',
+    'Villarreal': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Villarreal.png',
+    'Almería': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Almería.png',
+    'Real Madrid': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Real Madrid.png',
+    'Cádiz': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Cádiz.png',
+    'Real Sociedad': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Real Sociedad.png',
+    'Valencia': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Valencia.png',
+    'Barcelona': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Barcelona.png',
+    'Mallorca': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Mallorca.png',
+    'Granada': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Granada.png',
+    'Las Palmas': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Las Palmas.png',
+    'Osasuna': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Osasuna.png',
+    'Atlético Madrid': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Atlético Madrid.png',
+    'Rayo Vallecano': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Rayo Vallecano.png',
+    'Girona': 'https://raw.githubusercontent.com/Rako75/footballviz/main/La%20Liga%20Logos/Girona.png',
+    
+    # Ligue 1
+    'Marseille': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Marseille.png',
+    'Reims': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Reims.png',
+    'Lens': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Lens.png',
+    'Lorient': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Lorient.png',
+    'Nantes': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Nantes.png',
+    'Toulouse': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Toulouse.png',
+    'Montpellier': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Montpellier.png',
+    'Lyon': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Lyon.png',
+    'Monaco': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Monaco.png',
+    'Strasbourg': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Strasbourg.png',
+    'Nice': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Nice.png',
+    'Le Havre': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Le Havre.png',
+    'Clermont Foot': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Clermont Foot.png',
+    'Metz': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Metz.png',
+    'Brest': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Brest.png',
+    'Lille': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Lille.png',
+    'Paris S-G': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Paris S-G.png',
+    'Rennes': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Ligue%201%20Logos/Rennes.png',
+    
+    # Bundesliga
+    'Union Berlin': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Union Berlin.png',
+    'Eint Frankfurt': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Eintracht Frankfurt.png',
+    'Freiburg': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Freiburg.png',
+    'Köln': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Köln.png',
+    'Dortmund': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Dortmund.png',
+    'Leverkusen': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Leverkusen.png',
+    'Werder Bremen': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Werder Bremen.png',
+    'Mainz 05': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Mainz 05.png',
+    'Hoffenheim': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Hoffenheim.png',
+    'Wolfsburg': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Wolfsburg.png',
+    'Stuttgart': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Stuttgart.png',
+    'Bochum': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Bochum.png',
+    'Darmstadt 98': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Darmstadt 98.png',
+    'Bayern Munich': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Bayern Munich.png',
+    'Augsburg': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Augsburg.png',
+    'RB Leipzig': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/RB Leipzig.png',
+    'Heidenheim': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Heidenheim.png',
+    'Gladbach': 'https://raw.githubusercontent.com/Rako75/footballviz/main/Bundesliga%20Logos/Gladbach.png',
 }
 
-# Configuration des titres avec club et âge sous le nom du joueur
-title = dict(
-    title_name=f"{player1}",
-    title_color='#9B3647',
-    subtitle_name=f"{club1}, {age1} ans",
-    subtitle_color='#ABCDEF',
-    title_name_2=f"{player2}",
-    title_color_2='#3282b8',
-    subtitle_name_2=f"{club2}, {age2} ans",
-    subtitle_color_2='#ABCDEF',
-    title_fontsize=18,
-    subtitle_fontsize=15,
-)
+# Interface Streamlit pour afficher les résultats
+def show_radar_chart(position, league, team_name):
+    file_path = league_files[league][position]
+    data, stats_cols = load_and_preprocess_data(file_path, position)
+    
+    # Sélectionner les joueurs correspondant à l'équipe spécifiée
+    team_data = data[data['Club'] == team_name]
+    if team_data.empty:
+        st.write(f"Pas de données disponibles pour {team_name} dans cette position.")
+        return
+    
+    # Sélectionner le joueur avec les meilleures statistiques
+    best_player = team_data.loc[team_data[stats_cols[0]].idxmax()]
+    
+    # Charger les logos
+    logo_url = club_logos.get(team_name, None)
+    
+    # Créer et afficher le radar
+    radar = Radar(stats=stats_cols, stats_order=stats_cols)
+    fig = radar.plot(best_player[stats_cols], color='blue', label=best_player['Player'])
+    
+    if logo_url:
+        st.image(logo_url, width=50)
+    
+    st.pyplot(fig)
 
-# Affichage des logos des clubs
-st.image([club_logos.get(club1, ''), club_logos.get(club2, '')], width=50)
+# Paramètres Streamlit pour l'interface
+st.title("Football Radar")
+position = st.selectbox("Sélectionnez la position", ["Attaquant", "Milieu", "Défenseur"])
+league = st.selectbox("Sélectionnez la ligue", ["Premier League", "Bundesliga", "La Liga", "Ligue 1", "Serie A"])
+team_name = st.selectbox("Sélectionnez l'équipe", list(club_logos.keys()))
 
-# Note de bas de page
-endnote = "Source : FBref | Auteur : Alex Rakotomalala"
-
-# Instanciation de l'objet Radar
-radar = Radar(background_color="#121212", patch_color="#28252C", label_color="#F0FFF0", range_color="#F0FFF0")
-
-# Tracé du radar
-fig, ax = radar.plot_radar(
-    ranges=[(0, 100)] * len(params1),  # Les valeurs sont des pourcentages (0 à 100)
-    params=params1,
-    values=[player1_data, player2_data],
-    radar_color=['#9B3647', '#3282b8'],
-    title=title,
-    endnote=endnote,
-    alphas=[0.55, 0.5],
-    compare=True
-)
-
-# Affichage du radar dans Streamlit
-st.pyplot(fig)
+show_radar_chart(position, league, team_name)
