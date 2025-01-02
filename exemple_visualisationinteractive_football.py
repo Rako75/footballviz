3101b5490f8859933cc0c1c7510a8099aac1bade
Rako75 <147698888+Rako75@users.noreply.github.com>
2025-01-02 20:34:24 +0100

    Update exemple_visualisationinteractive_football.py

--- exemple_visualisationinteractive_football.py
+++ exemple_visualisationinteractive_football.py
@@ -139,14 +139,16 @@ fig, ax = radar.plot_radar(
     compare=True
 )
 
-# Ajout des logos des clubs
+# Ajout des logos des clubs (même zoom pour uniformité)
+zoom_factor = 0.2  # Facteur de zoom constant
+
 # Logo du club 1 (haut gauche)
-image1 = OffsetImage(club1_logo, zoom=0.2)
+image1 = OffsetImage(club1_logo, zoom=zoom_factor)
 annotation_box1 = AnnotationBbox(image1, (-2.0, 1.5), frameon=False)  # Position en haut à gauche
 ax.add_artist(annotation_box1)
 
 # Logo du club 2 (haut droite)
-image2 = OffsetImage(club2_logo, zoom=0.2)
+image2 = OffsetImage(club2_logo, zoom=zoom_factor)
 annotation_box2 = AnnotationBbox(image2, (2.0, 1.5), frameon=False)  # Position en haut à droite
 ax.add_artist(annotation_box2)
 
