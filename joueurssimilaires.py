10a5db2521e59308059362ec8f1fd7002a634e5a
Rako75 <147698888+Rako75@users.noreply.github.com>
2025-01-06 14:22:26 +0100

    Update joueurssimilaires.py

--- joueurssimilaires.py
+++ joueurssimilaires.py
@@ -13,7 +13,7 @@ def load_data():
 df = load_data()
 
 # Vérification des colonnes nécessaires
-required_columns = ['Joueur', 'Ligue', 'Equipe']
+required_columns = ['Joueur', 'Ligue', 'Equipe'] 
 if not all(col in df.columns for col in required_columns):
     raise ValueError(f"Les colonnes suivantes sont absentes du fichier CSV : {', '.join(required_columns)}")
 
@@ -140,5 +140,5 @@ if st.button("Trouver des joueurs similaires"):
         if similar_players:
             st.subheader(f"Joueurs similaires à {player_name} dans la ligue {selected_league} :")
             for i, (player, score, logo_url) in enumerate(similar_players, 1):
-                # Afficher le joueur, le score et le logo sur la même ligne
-                st.markdown(f"{i}. {player} (Score: {score:.2f}) ![Logo]({logo_url})", unsafe_allow_html=True)
+                # Afficher le joueur, le score et le logo sur la même ligne avec taille ajustée
+                st.markdown(f"{i}. {player} (Score: {score:.2f}) <img src='{logo_url}' width='30' height='30'>", unsafe_allow_html=True)
