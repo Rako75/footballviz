5fc023e30b7e476c2e9a129c568fbb58a17dae2a
Rako75 <147698888+Rako75@users.noreply.github.com>
2025-02-06 11:29:25 +0100

    Update PSGRacingBarChart.py

--- PSGRacingBarChart.py
+++ PSGRacingBarChart.py
@@ -8,14 +8,14 @@ file_path = "Meilleurs buteurs du PSG.xlsx"
 df = pd.read_excel(file_path, sheet_name="Feuil1")
 
 df = df.sort_values("Saison")
+df["Noms"] = df["Noms"].astype(str)
 
 def animate_chart():
-    fig = None
     for i in range(1, len(df) + 1):
         temp_df = df.iloc[:i]
-        fig = px.bar(temp_df, x="Buts", y="Noms", orientation="h", 
-                     text="Buts", title="Meilleurs buteurs du PSG par saison",
-                     animation_frame="Saison", range_x=[0, df["Buts"].max() + 5])
+        fig = px.bar(temp_df, x="Saison", y="Buts", text="Noms", 
+                     title="Meilleurs buteurs du PSG par saison",
+                     animation_frame="Saison", range_y=[0, df["Buts"].max() + 5])
         st.plotly_chart(fig)
         time.sleep(0.5)
 
