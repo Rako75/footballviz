fd1aaf5d58c110a998e4f097916a67b124d0de9e
Alex Rako <147698888+Rako75@users.noreply.github.com>
2025-03-11 09:59:12 +0100

    Update VisualisationsFootball.py

--- VisualisationsFootball.py
+++ VisualisationsFootball.py
@@ -3,6 +3,17 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-df = pd.read_csv('df_BIG2025.csv', delimiter=';', encoding='latin1')
+df = pd.read_csv("df_BIG2025.csv")  # Remplacez par votre fichier
 
 st.title("Football Statistics Dashboard")
+
+
+selected_league = st.selectbox("Selectionnez une ligue:", df['Compétition'].unique())
+filtered_df = df[df['Compétition'] == selected_league]
+
+
+# Visualization 1: Bar graph showing highest goal scorers (top 20)
+top_scorers = filtered_df.nlargest(20, 'Buts')
+fig1 = px.bar(top_scorers, x='Joueur', y='Buts', title='Top 20 meilleurs buteurs', labels={'Buts': 'Nombre de buts'})
+fig1.update_traces(hovertemplate='Joueur: %{x}<br>Buts: %{y}')
+st.plotly_chart(fig1)
