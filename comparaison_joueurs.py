d7f087aec23e4d6be9e426ee5cdbb909f7b94ca3
Alex Rako <147698888+Rako75@users.noreply.github.com>
2025-03-12 14:23:34 +0100

    Update comparaison_joueurs.py

--- comparaison_joueurs.py
+++ comparaison_joueurs.py
@@ -183,7 +183,7 @@ col1, col2, col3 = st.columns([5, 15, 5])  # Colonnes gauche, centre (radar), dr
 # Colonne 1 : Infos du joueur 1
 with col1:
     st.image(club1_logo, width=100)
-    st.subheader(player1)
+    st.subheader(f"{player1} ({'#9B3647'})")  # Ajout de la couleur entre parenthèses
     st.write(f"**Âge :** {player1_age}")
     st.write(f"**Titularisations :** {player1_titularisations}")
     st.write(f"**Buts :** {player1_buts}")
@@ -197,7 +197,7 @@ with col2:
 # Colonne 3 : Infos du joueur 2
 with col3:
     st.image(club2_logo, width=100)
-    st.subheader(player2)
+    st.subheader(f"{player2} ({'#3282b8'})")  # Ajout de la couleur entre parenthèses
     st.write(f"**Âge :** {player2_age}")
     st.write(f"**Titularisations :** {player2_titularisations}")
     st.write(f"**Buts :** {player2_buts}")
