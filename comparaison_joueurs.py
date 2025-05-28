3ea9859341ec14b2c5f22c9a0dec212aa64aa6fd
Alex Rako <147698888+Rako75@users.noreply.github.com>
2025-05-28 16:48:32 +0200

    Update comparaison_joueurs.py

--- comparaison_joueurs.py
+++ comparaison_joueurs.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-from mplsoccer import PyPizza
 import matplotlib.pyplot as plt
+from mplsoccer import PyPizza
 import requests
 from io import BytesIO
 
@@ -34,24 +34,50 @@ def load_and_preprocess_data(file_path, position):
         'Actions menant à un tir par 90 minutes':'Actions créant un tir p/90 min',
         'Somme des buts et passes attendues par 90 minutes':'xG + xAG p/90 min'
     })
+
     stats_cols = [col for col in stats_cols if col in data.columns]
-    data[col] = data[col].astype(float) / data['Matchs en 90 min']
-    stats_cols = [col for col in stats_cols if col in data.columns]
-    data[col] = (data[col].rank(pct=True) * 100).astype(int)
+
+    for col in stats_cols:
+        data[col] = data[col].astype(float) / data['Matchs en 90 min']
+
+    for col in stats_cols:
+        data[col] = (data[col].rank(pct=True) * 100).astype(int)
 
     return data, stats_cols
 
+# Fonction pour charger un logo à partir d'une URL
 def load_logo(logo_url):
     response = requests.get(logo_url)
     image = plt.imread(BytesIO(response.content), format='png')
     return image
 
+# Dictionnaires
 league_files = {
-    "Premier League": {"Attaquant": "Premier_League_Attaquant.csv", "Défenseur": "Premier_League_Défenseur.csv", "Milieu": "Premier_League_Milieu.csv"},
-    "Bundesliga": {"Attaquant": "Bundesliga_Attaquant.csv", "Défenseur": "Bundesliga_Défenseur.csv", "Milieu": "Bundesliga_Milieu.csv"},
-    "La Liga": {"Attaquant": "La_Liga_Attaquant.csv", "Défenseur": "La_Liga_Défenseur.csv", "Milieu": "La_Liga_Milieu.csv"},
-    "Ligue 1": {"Attaquant": "Ligue_1_Attaquant.csv", "Défenseur": "Ligue_1_Défenseur.csv", "Milieu": "Ligue_1_Milieu.csv"},
-    "Serie A": {"Attaquant": "Serie_A_Attaquant.csv", "Défenseur": "Serie_A_Défenseur.csv", "Milieu": "Serie_A_Milieu.csv"},
+    "Premier League": {
+        "Attaquant": "Premier_League_Attaquant.csv",
+        "Défenseur": "Premier_League_Défenseur.csv",
+        "Milieu": "Premier_League_Milieu.csv",
+    },
+    "Bundesliga": {
+        "Attaquant": "Bundesliga_Attaquant.csv",
+        "Défenseur": "Bundesliga_Défenseur.csv",
+        "Milieu": "Bundesliga_Milieu.csv",
+    },
+    "La Liga": {
+        "Attaquant": "La_Liga_Attaquant.csv",
+        "Défenseur": "La_Liga_Défenseur.csv",
+        "Milieu": "La_Liga_Milieu.csv",
+    },
+    "Ligue 1": {
+        "Attaquant": "Ligue_1_Attaquant.csv",
+        "Défenseur": "Ligue_1_Défenseur.csv",
+        "Milieu": "Ligue_1_Milieu.csv",
+    },
+    "Serie A": {
+        "Attaquant": "Serie_A_Attaquant.csv",
+        "Défenseur": "Serie_A_Défenseur.csv",
+        "Milieu": "Serie_A_Milieu.csv",
+    },
 }
 
 logo_directories = {
@@ -62,18 +88,18 @@ logo_directories = {
     "Serie A": "https://raw.githubusercontent.com/Rako75/footballviz/main/Serie%20A%20Logos",
 }
 
-# Streamlit app
-st.sidebar.title("Radarchart - Saison 24/25")
+# Streamlit interface
+st.sidebar.title("RadarChart - Saison 24/25")
 
 selected_position = st.sidebar.selectbox("Choisissez la position", options=["Attaquant", "Défenseur", "Milieu"])
-league1 = st.sidebar.selectbox("Sélectionnez la ligue du premier joueur", options=list(league_files.keys()))
-league2 = st.sidebar.selectbox("Sélectionnez la ligue du deuxième joueur", options=list(league_files.keys()))
+league1 = st.sidebar.selectbox("Ligue du premier joueur", options=list(league_files.keys()))
+league2 = st.sidebar.selectbox("Ligue du deuxième joueur", options=list(league_files.keys()))
 
 data1, params1 = load_and_preprocess_data(league_files[league1][selected_position], selected_position)
 data2, params2 = load_and_preprocess_data(league_files[league2][selected_position], selected_position)
 
-player1 = st.sidebar.selectbox("Sélectionnez le premier joueur", options=data1['Joueur'].unique())
-player2 = st.sidebar.selectbox("Sélectionnez le deuxième joueur", options=data2['Joueur'].unique())
+player1 = st.sidebar.selectbox("Premier joueur", options=data1['Joueur'].unique())
+player2 = st.sidebar.selectbox("Deuxième joueur", options=data2['Joueur'].unique())
 
 player1_data = data1[data1['Joueur'] == player1].iloc[0][params1].tolist()
 player2_data = data2[data2['Joueur'] == player2].iloc[0][params2].tolist()
@@ -85,71 +111,61 @@ age2 = int(data2[data2['Joueur'] == player2].iloc[0]['Âge'])
 
 club1_logo_url = f"{logo_directories[league1]}/{club1}.png"
 club2_logo_url = f"{logo_directories[league2]}/{club2}.png"
+
 club1_logo = load_logo(club1_logo_url)
 club2_logo = load_logo(club2_logo_url)
 
-# PyPizza Radar
+# Radar style
 slice_colors = ["#1A1A1A"] * len(params1)
 text_colors = ["#F0F0F0"] * len(params1)
-
 
 baker = PyPizza(
     params=params1,
     background_color="#121212",
-    straight_line_color="#FFFFFF",
+    straight_line_color="#F0F0F0",
     straight_line_lw=1,
     last_circle_lw=1,
+    last_circle_color="#F0F0F0",
     other_circle_lw=0,
-    inner_circle_size=20
+    inner_circle_size=10,
+    inner_circle_color="#1A1A1A",
+    direction_clockwise=True,
+    slice_colors=slice_colors,
+    value_colors=text_colors,
+    value_bck_colors=slice_colors,
+    param_location=110
 )
 
 fig, ax = baker.make_pizza(
     player1_data,
     compare_values=player2_data,
-    figsize=(8, 8),
+    figsize=(10, 10),
     color_blank_space="same",
-    slice_colors=slice_colors,
-    value_colors=["#9B3647", "#3282b8"],
-    value_bck_colors=["#9B364733", "#3282b833"],
-    blank_alpha=0.4,
-    param_location=110,
-    kwargs_slices=dict(edgecolor="#FFFFFF", zorder=2, linewidth=1),
-    kwargs_params=dict(color=text_colors, fontsize=12, va="center"),
-    kwargs_values=dict(fontsize=11, color=text_colors, zorder=3, bbox=dict(
-        edgecolor="none", facecolor="#000000", boxstyle="round,pad=0.2", lw=0)),
+    slice_colors=["#9B3647"] * len(params1),
+    value_colors=["white"] * len(params1),
+    value_bck_colors=["#9B3647"] * len(params1),
+    compare_slice_colors=["#3282b8"] * len(params1),
+    compare_value_colors=["white"] * len(params1),
+    compare_value_bck_colors=["#3282b8"] * len(params1),
+    kwargs_slices=dict(edgecolor="#F0F0F0", zorder=2, linewidth=1),
+    kwargs_compare=dict(edgecolor="#F0F0F0", zorder=2, linewidth=1),
+    kwargs_params=dict(color="white", fontsize=11, fontweight="bold"),
+    kwargs_values=dict(fontsize=10, color="white", fontweight="bold"),
+    kwargs_compare_values=dict(fontsize=10, color="white", fontweight="bold")
 )
 
-fig.suptitle("Comparaison des performances", color="#F0F0F0", fontsize=16)
+baker.add_title(f"{player1} vs {player2}", color="white", size=18)
+baker.add_credit("Données : FBref | Viz : @rakotomalala", color="gray", size=10)
 
-# Affichage dans Streamlit
+# Affichage
 col1, col2, col3 = st.columns([6, 15, 6])
-
-key_stat = {
-    "Attaquant": "xG p/90 min",
-    "Défenseur": "Interceptions",
-    "Milieu": "Passes progressives"
-}
-
-# Col 1 : joueur 1
 with col1:
     st.image(club1_logo, width=100)
     st.subheader(f"{player1} (rouge)")
     st.write(f"**Âge :** {age1}")
-    st.write(f"**Titularisations :** {int(data1[data1['Joueur'] == player1].iloc[0]['Titularisations'])}")
-    st.write(f"**Buts :** {int(data1[data1['Joueur'] == player1].iloc[0]['Buts'])}")
-    st.write(f"**Passes déc. :** {int(data1[data1['Joueur'] == player1].iloc[0]['Passes décisives'])}")
-    st.write(f"**{key_stat[selected_position]} :** {data1[data1['Joueur'] == player1].iloc[0][key_stat[selected_position]]}")
-
-# Col 2 : radar
 with col2:
     st.pyplot(fig)
-
-# Col 3 : joueur 2
 with col3:
     st.image(club2_logo, width=100)
     st.subheader(f"{player2} (bleu)")
     st.write(f"**Âge :** {age2}")
-    st.write(f"**Titularisations :** {int(data2[data2['Joueur'] == player2].iloc[0]['Titularisations'])}")
-    st.write(f"**Buts :** {int(data2[data2['Joueur'] == player2].iloc[0]['Buts'])}")
-    st.write(f"**Passes déc. :** {int(data2[data2['Joueur'] == player2].iloc[0]['Passes décisives'])}")
-    st.write(f"**{key_stat[selected_position]} :** {data2[data2['Joueur'] == player2].iloc[0][key_stat[selected_position]]}")
