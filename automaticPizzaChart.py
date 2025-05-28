99c8f6467533d1537e009c8270a6122b83af6a84
Alex Rako <147698888+Rako75@users.noreply.github.com>
2025-05-28 22:20:34 +0200

    Update automaticPizzaChart.py

--- automaticPizzaChart.py
+++ automaticPizzaChart.py
@@ -74,29 +74,29 @@ def link_generator(player_name, df):
     else:
         raise ValueError(f"Nom du joueur introuvable : {player_name}")
 
-def get_players_data(player_name):
-    link_to_player_profile = link_generator(player_name)
+def get_players_data(player_name, df):
+    link_to_player_profile = link_generator(player_name, df)
     html = urlopen("https://fbref.com" + link_to_player_profile)
     bs = BeautifulSoup(html, 'html.parser')
 
-    section_div = bs.find('div', {'class': 'section_heading_text'})
-    if not section_div:
-        raise ValueError(f"Section des stats non trouvée pour {player_name}")
+    scout_link = bs.find('div', {'class': 'section_heading_text'}).find('a')['href']
 
-    scout_anchor = section_div.find('a')
-    if not scout_anchor or 'href' not in scout_anchor.attrs:
-        raise ValueError(f"Lien scout non trouvé pour {player_name}")
 
-    scout_link = scout_anchor['href']
+
+
+
+
+
+
     scout_html = urlopen("https://fbref.com" + scout_link)
     bs_scout_all = BeautifulSoup(scout_html, 'html.parser')
     bs_scout = bs_scout_all.find('div', {'id': re.compile(r'div_scout_full_')})
-    if bs_scout is None:
-        raise ValueError(f"Aucune section de données de radar pour {player_name}")
+
+
 
     table = bs_scout.find("table", {'id': re.compile(r'scout_full_')})
-    if table is None:
-        raise ValueError(f"Aucune table de statistiques trouvée pour {player_name}")
+
+
 
     stat_keys, stat_values = [], []
     for row in table.find_all('tr'):
@@ -261,7 +261,7 @@ if selected_leagues:
 
     if player1:
         try:
-            keys1, values1 = get_players_data(player1)
+            keys1, values1 = get_players_data(player1, df_profiles)
             stats1 = dict(zip(keys1, values1))
             data1 = [float(stats1.get(s, "0").replace("%", "").strip() or 0) for s in selected_stats]
             df1 = pd.DataFrame([data1], columns=radar_labels)
