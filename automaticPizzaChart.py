31e6e56a6145f44e3df5e9b5cb37cb4d5058f66c
Alex Rako <147698888+Rako75@users.noreply.github.com>
2025-06-02 12:00:35 +0200

    Update automaticPizzaChart.py

--- automaticPizzaChart.py
+++ automaticPizzaChart.py
@@ -185,7 +185,7 @@ elif mode == "Radar comparatif":
         fig.text(0.515, 0.99, f"{joueur1} vs {joueur2}", size=24, ha="center",
                  fontproperties=font_bold.prop, color="#ffffff")
 
-        fig.text(0.515, 0.955, "Radar comparatif - Rang Percentile - Top 5 Championnat Européen | Saison 2024-25 | @AlexRakotomalala",
+        fig.text(0.515, 0.955, "Radar comparatif | Percentile | Saison 2024-25",
                  size=13, ha="center", fontproperties=font_bold.prop, color="#ffffff")
 
         legend_p1 = mpatches.Patch(color=COLOR_1, label=joueur1)
